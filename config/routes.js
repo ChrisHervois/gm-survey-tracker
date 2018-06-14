f35cfa01c887aecab41794a30d9599f8e2367d9b
Chris Hervois <chris.hervois@gmail.com>
2018-06-14 16:07:01 -0700

    add cors

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,15 @@ const root = require("../controllers/root.js")
 const survey = require("../controllers/survey.js")
 const confirm = require("../controllers/confirm.js")
 const authMiddleware = (req, res, next) => req.session.name ? next() : res.redirect('/');
+
+function youShallPass (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+};
+
 module.exports = function(app){
+  app.use(youShallPass)
   app.post('/appt.json', survey.incoming)
   app.get('/', root.index);
   app.post('/login', root.login)
